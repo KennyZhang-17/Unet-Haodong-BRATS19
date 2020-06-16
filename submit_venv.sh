3dbccbf75d6fa05468f5f23a5520623701a0656a
KennyZhang-17 <35554830+KennyZhang-17@users.noreply.github.com>
2020-06-16 15:51:44 -0700

    Add files via upload
    Batch Script Submission to Compute Canada run on BRATS 19 DataSet

---
+++ submit_venv.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --account=def-mhodaie
+#SBATCH --gres=gpu:p100:1
+#SBATCH --mem-per-cpu=32G
+#SBATCH --time=6-20:00
+
+module load python/3.7
+virtualenv --no-download $SLURM_TMPDIR/env
+source $SLURM_TMPDIR/env/bin/activate
+pip install --no-index --upgrade pip
+
+pip install --no-index -r requirements.txt
+
+pip install tensorlayer-1.6.5-py3-none-any.whl
+python train.py
